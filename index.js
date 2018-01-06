3b97b10d9a8f7fca5239759aca333e0245c6edf3
Denise Pendleton <dpendl1@hotmail.com>
2018-01-06 18:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  // return function() {
-  //   return "FUNKY!"
-  // }
+  return function() {
+    return "FUNKY!"
+  }
   
-  return "FUNKY!";
+  // return "FUNKY!";
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
